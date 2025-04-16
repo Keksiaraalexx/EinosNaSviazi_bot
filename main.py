be34bd256b9f8fbd7ed4d895db04b8cb23bf56c7
Keksiaraalexx <keksiara010@gmail.com>
2025-04-17 02:11:13 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import Message
+from aiogram.utils import executor
+from config import BOT_TOKEN, ADMIN_CHAT_ID
+from notifier import send_notification
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def start_handler(message: Message):
+    await message.reply("🔔 Эйнос на связи. Готов присылать уведомления!")
+    await send_notification(bot, ADMIN_CHAT_ID, "✨ Бот активирован и работает!")
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
